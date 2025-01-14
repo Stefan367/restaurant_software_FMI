92dc2dbc3fda1dd20fa4a65b0f41e3fafe1bf874
Stefan367 <stefan.ivanov673@gmail.com>
2025-01-15 01:29:58 +0200

    Create file

---
+++ restaurant_software_FMI/restaurant_software_FMI/restaurant_software_FMI.cpp
@@ -0,0 +1,11 @@
+// restaurant_software_FMI.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
+
